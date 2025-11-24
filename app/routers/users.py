9f04ea4a5de8341982141a9517bb45b5c29eaf5e
Ivan Deyev <deyevivan777@mail.ru>
2025-11-24 12:07:56 +0500

    migration (add column rating in products)

--- app/routers/users.py
+++ app/routers/users.py
@@ -6,7 +6,7 @@ from fastapi.security import OAuth2PasswordRequestForm
 from app.models.users import User as UserModel
 from app.schemas import UserCreate, User as UserSchema
 from app.db_depends import get_async_db
-from app.auth import hash_password, verify_password, create_access_token
+from app.auth import hash_password, verify_password, create_access_token, create_refresh_token
 
 import jwt
 from app.config import SECRET_KEY, ALGORITHM
