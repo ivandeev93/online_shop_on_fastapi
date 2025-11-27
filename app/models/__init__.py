d34b54949d80e44518273993202b45efacbab240
Ivan Deyev <deyevivan777@mail.ru>
2025-11-27 14:48:09 +0500

    created buyer bin

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,7 @@
 from .categories import Category
+from .cart_items import CartItem
 from .products import Product
 from .users import User
 
 
-__all__ = ["Category", "Product", "User"]
+__all__ = ["Category", "CartItem", "Product", "User"]
