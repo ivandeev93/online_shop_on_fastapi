c79971e5dc74d3bee150ca88783c1c79afb4da96
Ivan Deyev <deyevivan777@mail.ru>
2025-11-28 01:22:22 +0500

    added order system

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,6 +2,6 @@ from .categories import Category
 from .cart_items import CartItem
 from .products import Product
 from .users import User
+from .orders import Order, OrderItem
 
-
-__all__ = ["Category", "CartItem", "Product", "User"]
+__all__ = ["Category", "CartItem", "Order", "OrderItem", "Product", "User"]
